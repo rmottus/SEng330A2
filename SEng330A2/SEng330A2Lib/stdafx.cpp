14827f60a3c96569f7adc4936bd8cdab2f2a0c42
rmottus <rmottus@live.ca>
2015-11-15 16:21:01 -0800

    Added new library project so that source can be shared between main and test in the future. Removed unneded headers/cpp from main. Main no longer uses precompiled headers.

--- SEng330A2/SEng330A2Lib/stdafx.cpp
+++ SEng330A2/SEng330A2Lib/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// SEng330A2Main.pch will be the pre-compiled header
+// SEng330A2Lib.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
--- SEng330A2/SEng330A2Main/SEng330A2Main.cpp
+++ SEng330A2/SEng330A2Main/SEng330A2Main.cpp
@@ -1,7 +1,7 @@
 // SEng330A2Main.cpp : Defines the entry point for the console application.
 
 #include "stdafx.h"
-
+#include "tchar.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
